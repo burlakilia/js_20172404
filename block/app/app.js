8007c3adf25238dc6736ab89b479e3a5a3b6b543
Burlak Ilia <burlak@jivosite.com>
2017-06-22 22:41:46 +0300

    Добавлен компонент доски

---
+++ block/app/app.js
@@ -0,0 +1,10 @@
+(function () {
+
+    window.addEventListener('DOMContentLoaded', () => {
+        let Desc = window.Desc;
+
+        let desc = new Desc(document.querySelector('.js-desc'));
+    });
+
+
+})();
---
+++ block/desc/desc.js
@@ -0,0 +1,114 @@
+(function () {
+
+    let template = `
+           <table class="desc">
+                <tr>
+                    <td class="desc__cell"></td>
+                    <td class="desc__cell"></td>
+                    <td class="desc__cell"></td>
+                    <td class="desc__cell"></td>
+                    <td class="desc__cell"></td>
+                    <td class="desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                </tr>
+                <tr>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                </tr>
+                <tr>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                </tr>
+                                <tr>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                </tr>
+                <tr>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                </tr>
+                <tr>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                </tr>
+                <tr>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                </tr>
+                <tr>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                    <td class="desc__cell desc__cell"></td>
+                </tr>
+            </table> 
+    `;
+
+    class Desc {
+
+        constructor(node) {
+            this.node = node;
+            this.render();
+            this.node.addEventListener('click', event => this.onClick(event));
+        }
+
+        render() {
+            this.node.innerHTML = template;
+        }
+
+        onClick(event) {
+            let target = event.target;
+
+            if (this.current) {
+                this.current.classList.toggle('desc__cell_active', false);
+            }
+
+            target.classList.toggle('desc__cell_active');
+            this.current = target;
+        }
+
+    }
+
+    window.Desc = Desc;
+})();
